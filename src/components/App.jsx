9b8fd284bd75042ef6e3f442f3bb69b1a1bf2051
Praded3 <pradid.store@gmail.com>
2023-08-26 22:07:31 +0300

    12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,13 +9,35 @@ import { Contacts } from './Contacts/Contacts';
 export default class App extends Component {
 
   state = {
-    contacts: [
+    contacts: [],
+    filter: ''
+  }
+  componentDidMount() {
+    const getContacts = JSON.parse(localStorage.getItem('contacts'));
+    const stokContacts = [
       {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
       {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
       {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
       {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: ''
+    ]
+    
+    if (getContacts) {
+      this.setState({
+        contacts: getContacts,
+      })
+    } else {
+      this.setState({
+        contacts: stokContacts,
+      })
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    const { contacts: prevContacts } = prevState;
+    const { contacts: nextContacts } = this.state;    
+      if (prevContacts !== nextContacts) {
+        localStorage.setItem('contacts',  JSON.stringify(nextContacts))
+      }
   }
 
   handleSubmit = (values, { resetForm }) => {
