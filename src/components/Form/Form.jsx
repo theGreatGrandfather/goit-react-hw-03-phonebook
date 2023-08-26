eafbdfd1af83b5762fd60557a591e05ca2776650
Praded3 <pradid.store@gmail.com>
2023-08-26 15:16:55 +0300

    [

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from 'prop-types'
-import * as Yup from 'yup';
+import * as Yup from 'yup'
 import { Formik } from 'formik'
 
 import { AddBtn,StyledError, Label, MainForm, Input } from './Form.styled'
