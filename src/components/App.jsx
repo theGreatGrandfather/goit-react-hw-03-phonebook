c01c8eb0fdf0b9fc21e07443d33f6baf98676137
Praded3 <pradid.store@gmail.com>
2023-08-28 22:54:01 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,27 +9,21 @@ import { Contacts } from './Contacts/Contacts';
 export default class App extends Component {
 
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' }
+    ],
     filter: ''
   }
   componentDidMount() {
     const getContacts = JSON.parse(localStorage.getItem('contacts'));
-    const stokContacts = [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ]
-    
     if (getContacts) {
       this.setState({
         contacts: getContacts,
       })
-    } else {
-      this.setState({
-        contacts: stokContacts,
-      })
-    }
+    } 
   }
 
   componentDidUpdate(prevState) {
